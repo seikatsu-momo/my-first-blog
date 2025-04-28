78b5d630484d7a1e302b7b621d7886f9839c6e96
seikatsu-momo <takeru_momosawa@m-seikatsukankyo.or.jp>
2025-04-28 15:26:06 +0900

    My Kensaku app, chenge2

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
-from .local_settins import *
+#from .local_settins import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
