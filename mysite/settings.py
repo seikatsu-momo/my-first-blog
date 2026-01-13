a74a0aa0811e643983ecfd25d6a1b0301d549d79
takeru momosawa <takeru_momosawa@m-seikatsukankyo.or.jp>
2026-01-13 11:49:41 +0900

    app added kensaku_katasiki and pwa

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,13 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [BASE_DIR / "kensaku/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 try:
     from .local_settings import *
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import kensaku.views as kensaku
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('kensaku.urls'))
+    path('',include('kensaku.urls')),
+    path(
+        'manifest.json',
+        TemplateView.as_view(
+            template_name='manifest.json',
+            content_type='application/json',
+        ),
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATICFILES_DIRS[0],
+    )
