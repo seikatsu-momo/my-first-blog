c1d401c87706630bc98e4ca13ba1488e3ea3cdb4
momosawa <takeru_momosawa@m-seikatsukankyo.or.jp>
2026-03-03 10:36:00 +0900

    app added kensaku_katasiki and pwa new6

---
+++ kensaku/templates/serviceworker.js
@@ -0,0 +1,43 @@
+
+
+
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                '/',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+
+//オフラインキャッシュ
+const CACHE = 'v1';
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(c =>
+            c.addAll([
+                '/',
+                '/static/js/main.js',
+                '/static/css/style.css'
+            ])
+        )
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('kensaku.urls')),
