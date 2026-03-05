9eaffe2f16489f4eabfb1bce70ee7b9f91094751
momosawa <takeru_momosawa@m-seikatsukankyo.or.jp>
2026-03-05 09:01:03 +0900

    app added kensaku_katasiki and pwa new7

--- kensaku/serviceworker.js
+++ kensaku/serviceworker.js
@@ -2,25 +2,6 @@
 
 
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('v1').then(cache => {
-            return cache.addAll([
-                '/',
-            ]);
-        })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-
 //オフラインキャッシュ
 const CACHE_NAME = "kensaku-v1";
 
